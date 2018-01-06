e70fc9af26e3f55a532617b0ac7a254aa740c7d0
Daniel Hawton <daniel@hawton.com>
2018-01-05 19:36:28 -0900

    add nodes and services

--- main.go
+++ main.go
@@ -9,10 +9,22 @@ func main() {
 	go initWorker()
 
 	http.HandleFunc("/tasks", taskHandler)
+	http.HandleFunc("/nodes", nodeHandler)
+	http.HandleFunc("/services", serviceHandler)
 	http.ListenAndServe(":8081", nil)
 }
 
 func taskHandler(w http.ResponseWriter, r *http.Request) {
 	enc := json.NewEncoder(w)
 	enc.Encode(tasks)
+}
+
+func nodeHandler(w http.ResponseWriter, r *http.Request) {
+	enc := json.NewEncoder(w)
+	enc.Encode(nodes)
+}
+
+func serviceHandler(w http.ResponseWriter, r *http.Request) {
+	enc := json.NewEncoder(w)
+	enc.Encode(services)
 }
